a551e97866e0052a4971280e344509fc984537af
xorj <jorge.santos@dcomp.ufs.br>
2022-02-05 22:15:08 -0300

    Configura linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,26 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto"
+      }
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 };
