8d4d3ecc343192ed832626d7444b6bf78817ef68
xorj <jorge.santos@dcomp.ufs.br>
2022-02-06 16:01:57 -0300

    Adiciona botão de cadastro

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,28 @@
 <template>
   <div class="main-container">
     <b-row class="h-100 g-0">
-      <b-col cols="3" class="banner-transparencia" :style="bannerTransparencia">
-        <div class="banner-content text-center">
-          <b-img
-            :src="require('../assets/calendar.svg')"
-            class="calendar mb-4"
-          />
-          <h3 class="header text-white">Agendamento Online</h3>
-          <p class="text text-white">Rápido e seguro</p>
-          <p class="text text-gray">
-            Evite filas e aglomeração. <br />
-            O ser bem é o bem de todos
-          </p>
-          <b-button class="btn-transparencia mt-4">TRANSPARÊNCIA</b-button>
-        </div>
-        <div class="instituicoes">
-          <b-img :src="require('../assets/lais.svg')" />
-          <b-img :src="require('../assets/ufrn.svg')" />
+      <banner-transparencia />
+      <b-col cols="9" class="login-container">
+        <div class="link-cadastro-container p-3">
+          <div><p class="m-0">Não tem uma conta?</p></div>
+          <b-button class="btn-cadastro px-4 ml-3">
+            <b-img :src="require('../assets/user-icon.svg')" />
+            Crie uma
+          </b-button>
         </div>
       </b-col>
-      <b-col cols="9" class="login-container"> Login Container </b-col>
     </b-row>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-
-@Component({})
+import BannerTransparencia from "@/components/banner-transparencia/BannerTransparencia.vue";
+@Component({
+  components: {
+    BannerTransparencia,
+  },
+})
 export default class Login extends Vue {
   data() {
     return {
@@ -41,45 +35,19 @@ export default class Login extends Vue {
 </script>
 
 <style scoped>
-.calendar {
-  width: 4vw;
-  height: 4vw;
-}
-.text-gray {
-  color: #d2d2d2;
-}
-.header {
-  font-size: 1.5vw;
-  font-weight: 700;
-}
-.text {
-  font-size: 1vw;
-  font-weight: 300;
-}
 .main-container {
   height: 100%;
   width: 100%;
 }
-.banner-transparencia {
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+.login-container {
+}
+.btn-cadastro {
+  font-size: 16px;
   background-color: #3a0ca3;
+}
+.link-cadastro-container {
   display: flex;
-  flex-direction: column;
   justify-content: flex-end;
-}
-.banner-content {
-}
-.btn-transparencia {
-  color: white;
-  font-weight: 700;
-  font-size: 1vw;
-  padding: 10px 0;
-  background-color: #f72585;
-  width: 80%;
-}
-
-.instituicoes {
+  align-items: center;
 }
 </style>
