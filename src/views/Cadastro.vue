abf374b51ab692e459c5ffd7597a0bf1a704969e
xorj <jorge.santos@dcomp.ufs.br>
2022-02-06 19:49:07 -0300

    Adiciona nome ao componente da tela de cadastro

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -5,6 +5,8 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-@Component({})
+@Component({
+  name: "Cadastro",
+})
 export default class Cadastro extends Vue {}
 </script>
