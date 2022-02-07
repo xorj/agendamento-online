b6cb5e3b9b6a5d70d3037ffb4fb9086d2cb3ef40
xorj <jorge.santos@dcomp.ufs.br>
2022-02-07 16:25:39 -0300

    Passa a usar VeeValidate na aplicação

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue } from "bootstrap-vue";
+import * as VeeValidate from "vee-validate";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
+//@ts-ignore
+Vue.use(VeeValidate);
 
 Vue.config.productionTip = false;
 
