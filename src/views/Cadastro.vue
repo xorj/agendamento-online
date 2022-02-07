d9d20b688f984c113b6666123b1467456fd6e479
xorj <jorge.santos@dcomp.ufs.br>
2022-02-07 16:26:24 -0300

    Componentiza CardCadastro - adiciona validação dos campos

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -3,7 +3,7 @@
     <b-row class="h-100 g-0">
       <banner-transparencia />
       <b-col cols="9" class="d-flex align-items-center justify-content-center">
-        <div class="link-cadastro-container p-3">
+        <div class="link-login-container p-3">
           <div class="d-flex align-items-center">
             <p class="m-0">Não tem uma conta?</p>
           </div>
@@ -12,28 +12,8 @@
             Entrar
           </custom-button>
         </div>
-        <div class="card-login">
-          <b-card class="shadow-sm px-4 py-4">
-            <p class="text-dark-pink text-center">Preencha os campos abaixo</p>
-            <form>
-              <label for="email" class="text-dark-gray mb-1">Email</label>
-              <b-input
-                class="mb-2 px-3 py-4 input-border"
-                id="email"
-                type="email"
-              />
-              <label for="senha" class="text-dark-gray mb-1">Senha</label>
-              <b-input class="p-4 input-border" id="senha" type="password" />
-              <p class="text-end text-dark-gray my-4 mr-1">
-                Esqueceu sua senha?
-              </p>
-              <custom-button
-                color="secondary"
-                class="btn-entrar text-center w-100 py-3 mb-3"
-                >Entrar</custom-button
-              >
-            </form>
-          </b-card>
+        <div class="card-container">
+          <card-cadastro />
         </div>
         <div />
       </b-col>
@@ -45,48 +25,30 @@
 import { Component, Vue } from "vue-property-decorator";
 import BannerTransparencia from "@/components/banner-transparencia/BannerTransparencia.vue";
 import CustomButton from "@/components/custom-button/CustomButton.vue";
+import CardCadastro from "@/components/card-cadastro/CardCadastro.vue";
 
 @Component({
   name: "Cadastro",
   components: {
     BannerTransparencia,
     CustomButton,
+    CardCadastro,
   },
 })
 export default class Cadastro extends Vue {
-  goToCadastro(): void {
-    this.$router.push("/login");
+  goToLogin(): void {
+    this.$router.push("/");
   }
 }
 </script>
 
 <style scoped>
-.input-border {
-  border: 1px solid var(--light-gray);
-}
-.btn-entrar {
-  font-weight: 700;
-  color: white;
-}
-.login-card {
-  width: 450px;
-  max-width: 80vw;
-}
-
-.text-dark-gray {
-  color: var(--dark-gray);
-}
-
-.text-dark-pink {
-  color: var(--dark-pink);
-}
-
 .main-container {
   height: 100%;
   width: 100%;
   display: relative;
 }
-.card-login {
+.card-container {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -96,7 +58,7 @@ export default class Cadastro extends Vue {
   font-size: 1rem;
   background-color: var(--violet);
 }
-.link-cadastro-container {
+.link-login-container {
   display: flex;
   position: absolute;
   top: 0;
