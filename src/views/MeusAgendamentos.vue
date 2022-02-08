d429d78b43e3692ea01e0be141a6a934312de4ef
xorj <jorge.santos@dcomp.ufs.br>
2022-02-08 20:27:48 -0300

    Cria base da tela de Meus Agendamentos

--- src/views/MeusAgendamentos.vue
+++ src/views/MeusAgendamentos.vue
@@ -1,19 +1,62 @@
-<template><div class="content-wrapper">Meus Agendamentos</div></template>
-
+<template>
+  <div class="content-wrapper px-5 py-3">
+    <p class="title mb-5">Meus Agendamentos</p>
+    <div>
+      <p style="color: var(--violet)">Filtrar agendamento</p>
+      <div class="d-flex">
+        <b-img
+          :src="require('../assets/filter-icon.svg')"
+          class="filter pb-1"
+        />
+        <b-form-select
+          v-model="lugarFiltro"
+          :options="filtrosLugares"
+          class="ml-3 filtro-lugar"
+        >
+        </b-form-select>
+      </div>
+    </div>
+  </div>
+</template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({
   name: "MeusAgendamentos",
-  components: {},
 })
-export default class MeusAgendamentos extends Vue {}
+export default class MeusAgendamentos extends Vue {
+  lugarFiltro = "";
+  filtrosLugares = [
+    {
+      value: "teste",
+      text: "teste",
+    },
+    {
+      value: "teste",
+      text: "teste",
+    },
+    {
+      value: "teste",
+      text: "teste",
+    },
+  ];
+}
 </script>
 
 <style>
+.filtro-lugar {
+  width: 20rem;
+  border-color: var(--violet);
+  color: var(--violet);
+}
+.title {
+  font-size: 1.4rem;
+  font-weight: 700;
+  color: var(--violet);
+}
 .content-wrapper {
   height: 200%;
-  width: 100%;
+  width: auto;
   background-image: radial-gradient(
       120% 50% at top left,
       transparent 92%,
