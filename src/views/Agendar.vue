8eb19cce405f257f40537a4f6aa2158b26dcbdfa
xorj <jorge.santos@dcomp.ufs.br>
2022-02-08 18:28:16 -0300

    Adiciona componentes de Agendar e MeusAgendamentos

---
+++ src/views/Agendar.vue
@@ -0,0 +1,24 @@
+<template><div>Agendar</div></template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Agendar",
+  components: {},
+})
+export default class Agendar extends Vue {}
+</script>
+
+<style>
+.content-wrapper {
+  height: 200%;
+  width: 100%;
+  background-image: radial-gradient(
+      120% 50% at top left,
+      transparent 92%,
+      #f1f5f9 92.2%
+    ),
+    linear-gradient(135deg, white, white 100%);
+}
+</style>
---
+++ src/views/MeusAgendamentos.vue
@@ -0,0 +1,24 @@
+<template><div class="content-wrapper">Meus Agendamentos</div></template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "MeusAgendamentos",
+  components: {},
+})
+export default class MeusAgendamentos extends Vue {}
+</script>
+
+<style>
+.content-wrapper {
+  height: 200%;
+  width: 100%;
+  background-image: radial-gradient(
+      120% 50% at top left,
+      transparent 92%,
+      #f1f5f9 92.2%
+    ),
+    linear-gradient(135deg, white, white 100%);
+}
+</style>
