804f1bcabb28454e326cc05eb80921719ef69c6e
xorj <jorge.santos@dcomp.ufs.br>
2022-02-08 22:36:33 -0300

    Começa a criação da página de listagem de agendamentos

--- src/views/MeusAgendamentos.vue
+++ src/views/MeusAgendamentos.vue
@@ -3,11 +3,8 @@
     <p class="title mb-5">Meus Agendamentos</p>
     <div>
       <p style="color: var(--violet)">Filtrar agendamento</p>
-      <div class="d-flex">
-        <b-img
-          :src="require('../assets/filter-icon.svg')"
-          class="filter pb-1"
-        />
+      <div class="filtro-wrapper">
+        <b-img :src="require('../assets/filter-icon.svg')" class="filter" />
         <b-form-select
           v-model="lugarFiltro"
           :options="filtrosLugares"
@@ -15,35 +12,67 @@
         >
         </b-form-select>
       </div>
+      <div class="agendamentos-wrapper">
+        <card-agendamento
+          :key="agendamento.id"
+          v-for="agendamento in agendamentos"
+          :agendamento="agendamento"
+        />
+      </div>
+      <div class="d-flex justify-content-end">
+        <b-pagination></b-pagination>
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import CardAgendamento from "@/components/card-agendamento/CardAgedamento.vue";
+import IAgendamento from "@/interfaces/IAgendamento";
 
 @Component({
   name: "MeusAgendamentos",
+  components: {
+    CardAgendamento,
+  },
 })
 export default class MeusAgendamentos extends Vue {
-  lugarFiltro = "";
-  filtrosLugares = [
-    {
-      value: "teste",
-      text: "teste",
-    },
-    {
-      value: "teste",
-      text: "teste",
-    },
-    {
-      value: "teste",
-      text: "teste",
-    },
-  ];
+  lugarFiltro: string | null = null;
+  paginaAtual = 0;
+  agendamentos: Array<IAgendamento> = [];
+  filtrosLugares = [{ value: null, text: "Local de exame" }];
+
+  get agendamentosFiltrados(): Array<IAgendamento> {
+    if (this.lugarFiltro) {
+      return this.agendamentos.filter((agendamento: IAgendamento) => {
+        return agendamento.localizacao === this.lugarFiltro;
+      });
+    }
+    return this.agendamentos;
+  }
+
+  async created(): Promise<void> {
+    const token = this.$store.state.token;
+    this.agendamentos = await this.$store.dispatch("getAgendamentos", {
+      token: token,
+    });
+  }
 }
 </script>
 
 <style>
+.agendamentos-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: stretch;
+}
+.filtro-wrapper {
+  display: flex;
+  align-items: center;
+}
 .filtro-lugar {
   width: 20rem;
   border-color: var(--violet);
