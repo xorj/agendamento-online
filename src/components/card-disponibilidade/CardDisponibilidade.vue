3ba15c06c1542e7b806c1e8e05ff078460fd8c44
xorj <jorge.santos@dcomp.ufs.br>
2022-02-10 19:45:03 -0300

    Cria Componente CardDisponibilidade

---
+++ src/components/card-disponibilidade/CardDisponibilidade.vue
@@ -0,0 +1,240 @@
+<template>
+  <div class="card disponibilidade-card shadow p-4">
+    <p class="violet bold card-disponibilidade-title">
+      Locais de exame {{ dataFormatada }}
+    </p>
+    <div>
+      <div
+        v-for="disponibilidade in disponibilidadePaginaAtual"
+        :key="disponibilidade.id"
+      >
+        <p class="m-0 violet">
+          {{ disponibilidade.localizacao }} | Covid-19 | 8h às 16h
+        </p>
+        <div class="wrapper-cards-horarios">
+          <div
+            :key="disponibilidade.id + vaga[0]"
+            :class="[
+              'card-horario',
+              'rounded',
+              agendamentoSelecionado === disponibilidade.id &&
+              horarioSelecionado === vaga[0]
+                ? 'selecionado'
+                : '',
+            ]"
+            v-for="vaga in Object.entries(disponibilidade.vagas)"
+            @click="
+              () => {
+                (agendamentoSelecionado = disponibilidade.id),
+                  (horarioSelecionado = vaga[0]);
+              }
+            "
+          >
+            <div>
+              <p class="m-0">{{ gethoraFormatada(vaga[0]) }}</p>
+            </div>
+            <div
+              class="numero-vagas"
+              style="
+                background-color: var(--violet);
+                color: white;
+                border-radius: 0 3px 3px 0;
+              "
+            >
+              <p class="m-0">{{ vaga[1] }} vagas</p>
+            </div>
+          </div>
+        </div>
+        <hr style="background-color: var(--gray)" />
+      </div>
+    </div>
+
+    <div class="confirmar-pagination-wrapper mb-5 mt-4">
+      <c-button
+        :disabled="!horarioSelecionado"
+        class="rounded px-4 py-2 btn-confirmar"
+        color="secondary"
+        @click="agendarExame"
+        >Confirmar</c-button
+      >
+      <pagination
+        @changePagina="setPagina"
+        :paginaAtual="paginaAtual"
+        :totalDePaginas="totalDePaginas"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import CButton from "@/components/custom-button/CustomButton.vue";
+import Pagination from "@/components/pagination/Pagination.vue";
+
+interface IVagas {
+  "8h": number;
+  "9h": number;
+  "10h": number;
+  "11h": number;
+  "12h": number;
+  "13h": number;
+  "14h": number;
+  "15h": number;
+  "16h": number;
+}
+
+interface IDisponibilidade {
+  id: number;
+  municipio: string;
+  localizacao: string;
+  data: string;
+  vagas: IVagas;
+}
+
+interface IInfoAgendamento {
+  campanha: number | null;
+  municipio: string | null;
+  grupoDeAtendimento: number | null;
+  data: string;
+  tipoDeExame: string;
+}
+
+@Component({
+  name: "CardDisponibilidade",
+  components: {
+    CButton,
+    Pagination,
+  },
+})
+export default class CardDisponibilidade extends Vue {
+  @Prop() data!: string;
+  @Prop() infoAgendamento!: IInfoAgendamento;
+  @Prop() disponibilidadeAgendamento?: Array<IDisponibilidade>;
+
+  paginaAtual = 1;
+  agendamentoSelecionado = 0;
+  horarioSelecionado = "";
+
+  get disponibilidadePaginaAtual(): Array<IDisponibilidade> {
+    if (this.disponibilidadeAgendamento) {
+      console.log(
+        this.disponibilidadeAgendamento.slice(
+          (this.paginaAtual - 1) * 3,
+          (this.paginaAtual - 1) * 3 + 3
+        )
+      );
+      return this.disponibilidadeAgendamento.slice(
+        (this.paginaAtual - 1) * 3,
+        (this.paginaAtual - 1) * 3 + 3
+      );
+    } else {
+      return [];
+    }
+  }
+
+  get totalDePaginas(): number {
+    if (this.disponibilidadeAgendamento) {
+      return Math.ceil(this.disponibilidadeAgendamento?.length / 3);
+    } else {
+      return 0;
+    }
+  }
+
+  get dataFormatada(): string {
+    if (!this.data) {
+      return "";
+    }
+    let arrayData = this.data.split("-");
+    if (arrayData.length === 3) {
+      return `- ${arrayData[2]}/${arrayData[1]}/${arrayData[0]}`;
+    } else {
+      return "";
+    }
+  }
+
+  async agendarExame(): Promise<void> {
+    let { grupoDeAtendimento, tipoDeExame } = this.infoAgendamento;
+
+    const localEscolha = this.disponibilidadeAgendamento?.find(
+      (disponivel: IDisponibilidade) => {
+        return disponivel.id === this.agendamentoSelecionado;
+      }
+    );
+    if (localEscolha) {
+      let { data, municipio, localizacao } = localEscolha;
+      let informacoesAgendamento = {
+        usuario_id: this.$store.state.user.id,
+        campanha_id: 1, //COVID-19
+        grupo_atendimento_id: grupoDeAtendimento,
+        municipio: municipio,
+        localizacao: localizacao,
+        data: data,
+        hora: this.gethoraFormatada(this.horarioSelecionado),
+        status: "AGENDADO",
+        tipo_exame: tipoDeExame,
+      };
+      await this.$store.dispatch("agendarExame", {
+        ...informacoesAgendamento,
+      });
+    }
+  }
+
+  setPagina(pagina: number): void {
+    this.paginaAtual = pagina;
+  }
+
+  gethoraFormatada(hora: string): string {
+    let H = hora.split("h")[0];
+    return `${H}:00`;
+  }
+}
+</script>
+
+<style scoped>
+.btn-confirmar {
+  width: 200px;
+}
+.confirmar-pagination-wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+.card-horario > div {
+  height: 100%;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.card-horario {
+  cursor: pointer;
+  border: 1px solid var(--violet);
+  display: flex;
+  min-height: 30px;
+}
+.card-horario.selecionado {
+  border: 1px solid var(--dark-pink);
+}
+.card-horario.selecionado > div.numero-vagas {
+  background-color: var(--dark-pink) !important;
+}
+
+.wrapper-cards-horarios {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+}
+
+.card-disponibilidade-title {
+  font-size: 1.3rem;
+}
+.violet {
+  color: var(--violet);
+}
+.bold {
+  font-weight: 700;
+}
+.disponibilidade-card {
+  margin-left: 450px;
+}
+</style>
