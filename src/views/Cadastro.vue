b12fc971354654253eee0c1feb0f5988618e6fc6
xorj <jorge.santos@dcomp.ufs.br>
2022-02-11 18:42:27 -0300

    Adiciona modal de termos de uso e política de privacidade

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -7,31 +7,94 @@
           <div class="d-flex align-items-center">
             <p class="m-0">Já tem conta?</p>
           </div>
-          <custom-button @click="goToLogin" class="btn-cadastro px-4 py-2 ml-3">
+          <c-button @click="goToLogin" class="btn-cadastro px-4 py-2 ml-3">
             <b-img :src="require('../assets/user-icon.svg')" />
             Entrar
-          </custom-button>
+          </c-button>
         </div>
         <div class="card-container">
           <card-cadastro />
+          <p class="p-2 text-center">
+            Ao assinar você concorda com os
+            <span v-b-modal.modal> <u>termos de serviço</u></span> e
+            <span v-b-modal.modal><u>política de privacidade</u></span>
+          </p>
         </div>
         <div />
       </b-col>
     </b-row>
+    <b-modal
+      id="modal"
+      no-title
+      hide-header
+      hide-header-close
+      size="lg"
+      centered
+    >
+      <div class="p-4">
+        <p class="dark-pink bold title-modal">
+          Termos e Política de privacidade
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+          amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+          diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+          et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+          amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+          diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+          et ea rebum. Stet clita kasd
+        </p>
+      </div>
+      <template
+        v-slot:modal-footer="{ hide }"
+        class="px-4 py-3"
+        style="margin: 0"
+      >
+        <c-button
+          variant="primary"
+          class="btn-fechar-termos py-2 px-5 rounded"
+          @click="hide()"
+          >Fechar</c-button
+        >
+      </template>
+    </b-modal>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import BannerTransparencia from "@/components/banner-transparencia/BannerTransparencia.vue";
-import CustomButton from "@/components/custom-button/CustomButton.vue";
+import CButton from "@/components/custom-button/CustomButton.vue";
 import CardCadastro from "@/components/card-cadastro/CardCadastro.vue";
 
 @Component({
   name: "Cadastro",
   components: {
     BannerTransparencia,
-    CustomButton,
+    CButton,
     CardCadastro,
   },
 })
@@ -42,7 +105,26 @@ export default class Cadastro extends Vue {
 }
 </script>
 
-<style scoped>
+<style>
+.modal-footer {
+  background-color: #f9fafb;
+}
+
+.btn-fechar-termos {
+  border: 1px solid var(--light-gray);
+  background-color: white !important;
+  border-color: #ababab !important;
+  color: var(--dark-gray) !important;
+}
+.modal-body {
+  font-family: "Poppins", Arial, Helvetica, sans-serif !important;
+  padding: 0;
+}
+.title-modal {
+  font-size: 1.3rem;
+  color: var(--dark-pink) !important;
+  font-weight: 700;
+}
 .main-container {
   height: 100%;
   width: 100%;
@@ -53,6 +135,8 @@ export default class Cadastro extends Vue {
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  width: 450px;
+  max-width: 80vw;
 }
 .btn-cadastro {
   font-size: 1rem;
