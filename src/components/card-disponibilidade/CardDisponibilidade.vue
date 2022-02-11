5d1503f7dcd8386af96915d69c859204af7506f1
xorj <jorge.santos@dcomp.ufs.br>
2022-02-10 21:18:31 -0300

    Remove variável sem uso

--- src/components/card-disponibilidade/CardDisponibilidade.vue
+++ src/components/card-disponibilidade/CardDisponibilidade.vue
@@ -118,7 +118,6 @@ export default class CardDisponibilidade extends Vue {
   @Prop() infoAgendamento!: IInfoAgendamento;
   @Prop() disponibilidadeAgendamento?: Array<IDisponibilidade>;
 
-  showComprovante = false;
   agendamento = {};
 
   paginaAtual = 1;
