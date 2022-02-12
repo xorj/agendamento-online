6b2daaa33de9691e595ea06af5c5feea6ee4f6ba
xorj <jorge.santos@dcomp.ufs.br>
2022-02-11 22:41:01 -0300

    Adiciona gráficos necessários para os filtros

---
+++ src/components/charts/GruposAtendimento.vue
@@ -0,0 +1,27 @@
+<script>
+import { Pie } from "vue-chartjs";
+
+export default {
+  extends: Pie,
+  name: "GruposAtendimento",
+  props: ["data"],
+  watch: {
+    data() {
+      this.renderChart(this.data, {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          fontStyle: "light",
+          text: "Grupos de Atendimento",
+          fontColor: "#4a4a4a",
+          fontFamily: "'Poppins', Arial, Helvetica, sans-serif",
+        },
+      });
+    },
+  },
+};
+</script>
---
+++ src/components/charts/TiposExame.vue
@@ -0,0 +1,27 @@
+<script>
+import { Pie } from "vue-chartjs";
+
+export default {
+  extends: Pie,
+  name: "TiposExame",
+  props: ["data"],
+  watch: {
+    data() {
+      this.renderChart(this.data, {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          fontFamily: "'Poppins', Arial, Helvetica, sans-serif",
+        },
+        title: {
+          display: true,
+          fontStyle: "light",
+          text: "Tipos de Exame",
+          fontColor: "#4a4a4a",
+          fontFamily: "'Poppins', Arial, Helvetica, sans-serif",
+        },
+      });
+    },
+  },
+};
+</script>
