35e02abb2a79e5d34af7b93f349b10bb13778291
xorj <jorge.santos@dcomp.ufs.br>
2022-02-12 20:46:39 -0300

    Organiza estilização do app

--- src/App.vue
+++ src/App.vue
@@ -5,42 +5,7 @@
 </template>
 
 <style>
-:root {
-  --red: #ff4646;
-  --red-cancelado: #dc2626;
-  --violet: #3a0ca3;
-  --dark-pink: #f72585;
-  --dark-gray: #4a4a4a;
-  --transparent-gray: #92929257;
-  --light-gray: #b4b4b4;
-  --green: #00cf9f;
-}
-
-#app {
-  font-family: "Poppins", Arial, Helvetica, sans-serif;
-}
-
-.btn:focus,
-.btn:active:focus,
-.btn.active:focus,
-.btn.focus,
-.btn:active.focus,
-.btn.active.focus {
-  outline: none !important;
-  box-shadow: none !important;
-}
-.btn {
-  font-size: 1rem;
-  box-shadow: none !important;
-  color: white;
-  font-weight: 700;
-}
-.btn-primary {
-  background-color: var(--violet) !important;
-  border-color: var(--violet) !important;
-}
-.btn-secondary {
-  background-color: var(--dark-pink) !important;
-  border-color: var(--dark-pink) !important;
-}
+@import "./css/colors.css";
+@import "./css/button-style.css";
+@import "./css/button-remove-outline.css";
 </style>
