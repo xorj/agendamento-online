a3cef2229a7a3a77bcc0653e2dd28eef86db1c10
xorj <jorge.santos@dcomp.ufs.br>
2022-02-11 21:27:11 -0300

    Adiciona menu de filtro

--- src/views/Transparencia.vue
+++ src/views/Transparencia.vue
@@ -2,29 +2,83 @@
   <div class="main-container">
     <b-row class="h-100 g-0">
       <banner-transparencia />
-      <b-col cols="9" class="px-5 py-3">
+      <b-col class="px-5 py-3">
         <p class="title bold violet">Transparência</p>
-        <div class="charts-container mt-5">
-          <div class="chart-container card shadow-sm p-3">
-            <doses-recebidas :data="dataDosesRecebidas" />
+        <template v-if="!filtroOpen">
+          <div class="charts-container mt-5">
+            <div class="chart-container card shadow-sm p-3">
+              <grafico-pizza
+                :data="dataDosesRecebidas"
+                titulo="Doses recebidas"
+              />
+            </div>
+            <div class="chart-container card shadow-sm p-3">
+              <doses-aplicadas :data="dataDosesAplicadas" />
+            </div>
+            <div class="chart-container card shadow-sm p-3">
+              <vacinacao-geral :data="dataVacinacaoGeral" />
+            </div>
           </div>
-          <div class="chart-container card shadow-sm p-3">
-            <doses-aplicadas :data="dataDosesAplicadas" />
+        </template>
+        <template v-if="filtroOpen">
+          <div class="charts-container mt-5">
+            <div class="chart-container card shadow-sm p-3">GRAFICO</div>
           </div>
-          <div class="chart-container card shadow-sm p-3">
-            <vacinacao-geral :data="dataVacinacaoGeral" />
+        </template>
+      </b-col>
+      <b-col v-if="filtroOpen" cols="3" class="wrapper-filtro p-3">
+        <p style="color: white; font-weight: 600" class="mb-0">Filtros</p>
+        <b-form-input class="my-4" placeholder="Pesquise aqui" type="text" />
+        <div class="p-3 minicipio-wrapper rounded mb-3">
+          <p class="mb-0" style="color: white">Município</p>
+        </div>
+        <b-form-radio-group id="radio-local" v-model="filtroMunicipio">
+          <div class="radio-wrapper p-2">
+            <b-form-radio
+              :key="municipio"
+              v-for="municipio in optionsMunicipios"
+              :value="municipio"
+              ><p style="color: white">
+                {{ municipio }}
+              </p></b-form-radio
+            >
           </div>
+        </b-form-radio-group>
+        <div class="button-wrapper" v-if="filtroOpen">
+          <custom-button
+            class="px-3 py-2 btn-cancelar"
+            @click="
+              () => {
+                filtroOpen = false;
+              }
+            "
+            color="primary"
+            >Cancelar</custom-button
+          >
+          <custom-button class="px-3 py-2 btn-filtrar" color="primary"
+            >Filtrar</custom-button
+          >
         </div>
       </b-col>
     </b-row>
-    <div class="link-voltar-filtrar-container p-3">
+    <div class="link-voltar-filtrar-container p-3" v-if="!filtroOpen">
+      <custom-button
+        class="px-5 py-2 mr-2"
+        @click="
+          () => {
+            filtroOpen = true;
+          }
+        "
+        >Filtrar
+      </custom-button>
+
       <custom-button
         @click="
           () => {
             hasHistory() ? $router.go(-1) : $router.push('/');
           }
         "
-        class="btn-voltar px-4 py-2 ml-3"
+        class="btn-voltar px-5 py-2"
       >
         Voltar
       </custom-button>
@@ -37,7 +91,7 @@ import { Component, Vue } from "vue-property-decorator";
 import BannerTransparencia from "@/components/banner-transparencia/BannerTransparencia.vue";
 import CustomButton from "@/components/custom-button/CustomButton.vue";
 import DosesRecebidas from "@/components/charts/DosesRecebidas.vue";
-import DosesAplicadas from "@/components/charts/DosesAplicadas.vue";
+import GraficoPizza from "@/components/charts/GraficoPizza.vue";
 import VacinacaoGeral from "@/components/charts/VacinacaoGeral.vue";
 
 @Component({
@@ -46,7 +100,7 @@ import VacinacaoGeral from "@/components/charts/VacinacaoGeral.vue";
     BannerTransparencia,
     CustomButton,
     DosesRecebidas,
-    DosesAplicadas,
+    GraficoPizza,
     VacinacaoGeral,
   },
 })
@@ -54,6 +108,13 @@ export default class Transparencia extends Vue {
   dataDosesRecebidas = {};
   dataDosesAplicadas = {};
   dataVacinacaoGeral = {};
+  dataAgendamentoTipoExame = {};
+  dataGrupoDeAtendimento = {};
+
+  optionsMunicipios = [];
+  filtroMunicipio = "";
+
+  filtroOpen = false;
 
   bannerTransparencia = {
     backgroundImage: `url(${require("../assets/banner-transparencia.png")})`,
@@ -113,6 +174,40 @@ export default class Transparencia extends Vue {
     };
   }
 
+  somaTiposExame(tipoDeExame: any, agendamentos: any): number {
+    let cont = 0;
+    agendamentos.forEach((agendamento: any) => {
+      if (agendamento.tipo_exame === tipoDeExame) {
+        cont++;
+      }
+    });
+    return cont;
+  }
+
+  setDataAgendamentoTipoExame(agendamentos: any): void {
+    let tiposDeExame: Array<string> = [];
+    agendamentos.forEach((agendamentos: any) => {
+      tiposDeExame = tiposDeExame.concat(agendamentos.tipo_exame);
+    });
+    tiposDeExame = [...new Set(tiposDeExame)];
+    let contExames = tiposDeExame.map((tipoDeExame: string) => {
+      return {
+        [tipoDeExame]: this.somaTiposExame(tipoDeExame, agendamentos),
+      };
+    });
+    this.dataAgendamentoTipoExame = {
+      labels: Object.keys(contExames).map(this.capitalize),
+      datasets: [
+        {
+          label: "Doses Recebidas por fabricante",
+          data: Object.values(contExames),
+          backgroundColor: ["#3a0ca3", "#f72585"],
+          hoverOffset: 4,
+        },
+      ],
+    };
+  }
+
   setDataVacinacaoGeral(geral: any): void {
     let data = [0, 0, 0];
     geral.forEach((faixa: any) => {
@@ -131,6 +226,17 @@ export default class Transparencia extends Vue {
     };
   }
 
+  async getMunicipios(): Promise<void> {
+    const agendamentos = await this.$store.dispatch("getAgendamentos", {
+      municipio: this.filtroMunicipio,
+    });
+    let municipios = agendamentos.map((agendamentos: any) => {
+      return agendamentos.municipio;
+    });
+    municipios = [...new Set(municipios)];
+    this.optionsMunicipios = municipios;
+  }
+
   async created() {
     let data = await this.$store.dispatch("getTransparencia");
     const { recebidas, aplicadas } = data.doses;
@@ -138,11 +244,40 @@ export default class Transparencia extends Vue {
     this.setDataDosesAplicadas(aplicadas);
     const geral = data.vacinacao;
     this.setDataVacinacaoGeral(geral);
+    this.getMunicipios();
+    let agendamentos = await this.$store.dispatch("getAgendamentos");
+    this.setDataAgendamentoTipoExame(agendamentos);
   }
 }
 </script>
 
-<style scoped>
+<style>
+.custom-control-input:checked ~ .custom-control-label::before {
+  color: #fff;
+  border-color: var(--dark-pink) !important;
+  background-color: var(--dark-pink) !important;
+}
+.btn-cancelar {
+  color: var(--violet);
+  background-color: white !important;
+}
+.btn-filtrar {
+  color: var(--violet);
+  background-color: var(--dark-pink) !important;
+}
+.button-wrapper {
+  display: flex;
+  justify-content: space-around;
+  margin-top: auto;
+}
+.minicipio-wrapper {
+  background-color: var(--dark-pink);
+}
+.wrapper-filtro {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--violet);
+}
 .title {
   font-size: 1.3rem;
   font-weight: 700;
